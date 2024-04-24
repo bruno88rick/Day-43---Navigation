67b25224ca0fedd21821130eebd57100fbcb4594
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-04-24 05:32:37 -0300

    Initial Commit

---
+++ Day 43 - Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day 43 - Navigation
+//
+//  Created by Bruno Oliveira on 24/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
